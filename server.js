17dff790b0fc333ac55e60117f67d47ac77e9933
BJ Clark <bjclark13@outlook.com>
2021-02-22 20:25:38 -0500

    Add cors

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-
+const cors = require("cors");
 // Allows you to use path params, body, and
 // query string parameters
 app.use(express.json());
 
 app.use(cors());
 
-const port = 3000
+const port = 3000;
 
 // Import routes file, this is where
 // the API logic will go
-const routes = require('./saved.js');
-
+const routes = require("./saved.js");
 
 // the routes module will serve
 // the API from /my-routes
-app.use('/api/quotes', routes);
+app.use("/api/quotes", routes);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
