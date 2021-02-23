b0c1716931fd887c4e3e61cb74e957ddb95bac78
BJ Clark <bjclark13@outlook.com>
2021-02-22 19:46:25 -0500

    Add some more test stuff

--- src/app/ron-swanson.service.ts
+++ src/app/ron-swanson.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { Quote } from "./quote";
 
 @Injectable({
   providedIn: "root",
@@ -7,6 +8,13 @@ export class RonSwansonService {
   // import http client here
   constructor() {}
 
+  quotes: Quote[] = [
+    { id: 0, quote: "Todo: " },
+    { id: 1, quote: "Replace" },
+    { id: 2, quote: "with" },
+    { id: 3, quote: "API call." },
+  ];
+
   getRandomQuote(): string {
     // use http client
     // to request Ron Swanson Quote from
@@ -14,6 +22,8 @@ export class RonSwansonService {
     return "";
   }
 
+  // TODO 
+  // Replace this with a GET call to your API
   getSavedQuotes(): string[] {
     // use http client
     // to get saved quotes from
@@ -21,5 +31,23 @@ export class RonSwansonService {
     // aka
     // http://localhost:3000/api/quotes
     return [];
+  }
+
+  // TODO
+  // Repalce this with a POST call to your api
+  saveQuote(quote: string) {
+    this.quotes.push({ quote, id: this.quotes.length });
+  }
+
+  // TODO: 
+  // Replace this with a DELETE call to your api
+  deleteQuote(id: number) {
+
+    const index = this.quotes.findIndex( (quote) => {
+      console.log(quote.id)
+      return quote.id == id;
+    });
+
+    this.quotes.splice(index, 1);
   }
 }
