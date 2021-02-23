e7c6f98297015970f66b7f3b8512613c7a256451
BJ Clark <bjclark13@outlook.com>
2021-02-22 19:50:51 -0500

    Add HTTP call to ron swanson API

--- src/app/ron-swanson.service.ts
+++ src/app/ron-swanson.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Quote } from "./quote";
 
 @Injectable({
   providedIn: "root",
@@ -8,6 +9,13 @@ export class RonSwansonService {
   // import http client here
   constructor(private http: HttpClient) {}
 
+  quotes: Quote[] = [
+    { id: 0, quote: "Todo: " },
+    { id: 1, quote: "Replace" },
+    { id: 2, quote: "with" },
+    { id: 3, quote: "API call." },
+  ];
+
   getRandomQuote() {
     // use http client
     // to request Ron Swanson Quote from
@@ -15,6 +23,8 @@ export class RonSwansonService {
     return this.http.get('http://ron-swanson-quotes.herokuapp.com/v2/quotes');
   }
 
+  // TODO 
+  // Replace this with a GET call to your API
   getSavedQuotes(): string[] {
     // use http client
     // to get saved quotes from
@@ -22,5 +32,23 @@ export class RonSwansonService {
     // aka
     // http://localhost:3000/api/quotes
     return [];
+  }
+
+  // TODO
+  // Repalce this with a POST call to your api
+  saveQuote(quote: string) {
+    this.quotes.push({ quote, id: this.quotes.length });
+  }
+
+  // TODO: 
+  // Replace this with a DELETE call to your api
+  deleteQuote(id: number) {
+
+    const index = this.quotes.findIndex( (quote) => {
+      console.log(quote.id)
+      return quote.id == id;
+    });
+
+    this.quotes.splice(index, 1);
   }
 }
